6b3c6b7ced07081b0351a4b74da5c784b3a552d0
Sawon Mukherjee <125810951+Sawon76@users.noreply.github.com>
2024-09-08 22:29:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
             const playlistURLs = {
                 'mechanical': 'https://www.youtube.com/playlist?list=PLiSPNzs4fD9tiqHO5zEn4PqHmsrWsh6UI&si=hyz6pPyrBohRMs7h',
                 'data-analysis': 'https://www.youtube.com/playlist?list=PLU6SqdYcYsfJPF-4HphQQ8OceDtqhlSW8&si=Koo67Tvyb9BGDvFS',
-                'cad': 'https://www.youtube.com/playlist?list=PLU6SqdYcYsfJPF-4HphQQ8OceDtqhlSW8&si=Koo67Tvyb9BGDvFS',
+                'cad': 'https://youtube.com/playlist?list=PLv2ZxY8SkeQWF3YdaR0b7EDH8TIUpWw7_&si=EmMWjy05zFR9ILSa',
                 
             };
 
