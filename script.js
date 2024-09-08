35a2658080242e54b4b6b9f9fd161d14b8a97c77
Sawon76 <125810951+Sawon76@users.noreply.github.com>
2024-09-08 17:31:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function redirectToYoutube(url) {
-    window.location.href = url;
-}
+ function openPlaylist(course) {
+            // Replace these URLs with actual playlist links
+            const playlistURLs = {
+                'mechanical': 'https://www.youtube.com/playlist?list=PLiSPNzs4fD9tiqHO5zEn4PqHmsrWsh6UI&si=hyz6pPyrBohRMs7h',
+                'data-analysis': 'https://www.youtube.com/playlist?list=PLU6SqdYcYsfJPF-4HphQQ8OceDtqhlSW8&si=Koo67Tvyb9BGDvFS',
+                'cad': 'https://www.youtube.com/playlist?list=PLU6SqdYcYsfJPF-4HphQQ8OceDtqhlSW8&si=Koo67Tvyb9BGDvFS',
+                
+            };
+
+            window.open(playlistURLs[course], '_blank');
+        }
+
+        
