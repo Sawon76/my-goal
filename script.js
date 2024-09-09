49fa4577ec8f87fd26631f8887fd0f1dc54df9bd
Sawon Mukherjee <125810951+Sawon76@users.noreply.github.com>
2024-09-09 14:42:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
                 'mechanical': 'https://www.youtube.com/playlist?list=PLiSPNzs4fD9tiqHO5zEn4PqHmsrWsh6UI&si=hyz6pPyrBohRMs7h',
                 'data-analysis': 'https://www.youtube.com/playlist?list=PLU6SqdYcYsfJPF-4HphQQ8OceDtqhlSW8&si=Koo67Tvyb9BGDvFS',
                 'cad': 'https://youtube.com/playlist?list=PLv2ZxY8SkeQWF3YdaR0b7EDH8TIUpWw7_&si=EmMWjy05zFR9ILSa',
+                'html': 'https://youtube.com/playlist?list=PLtUrM4epv9fjftaPCCF8Xx6jjb3_WA2Uo&si=7Uf8lRkO2NyENgF4',
                 
             };
 
